aeaa7d5c8160627c1564a6672e3ba18d9df97d3f
Fatemeh Rostamipour <fatemehrostamih987@gmail.com>
2024-05-16 17:04:29 +0330

    Step01-Task11-BubbleSort

---
+++ Step01/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,41 @@
+﻿
+List<int> list = new List<int>();
+
+Console.WriteLine("Please enter the number of elements :");
+int numbers = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the Elements : ");
+for (int i = 0; i < numbers; i++)
+{
+    list.Add(Int32.Parse(Console.ReadLine()));
+}
+
+BubbleSort(list);
+
+#region [- BubbleSort(List<int> input) -]
+void BubbleSort(List<int> input)
+{
+    int temporary;
+
+    for (int j = 0; j <= input.Count - 2; j++)
+    {
+        for (int i = 0; i <= input.Count - 2; i++)
+        {
+            if (list[i] > list[i + 1])
+            {
+                temporary = input[i + 1];
+                input[i + 1] = input[i];
+                input[i] = temporary;
+            }
+        }
+    }
+
+    Console.WriteLine("Sorted:");
+    foreach (int p in input)
+    {
+        Console.Write($"{p} ");
+    }
+} 
+#endregion
+
+Console.ReadLine();
